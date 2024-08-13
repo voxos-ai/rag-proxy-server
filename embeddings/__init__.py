93043044e792e9ef78df81e6715dd8fe8389b733
Marmik Pandya <marmikpandya@gmail.com>
2024-08-13 19:49:07 +0530

    remove ingestion server dependency

--- embeddings/__init__.py
+++ embeddings/__init__.py
@@ -1,7 +1,7 @@
 from typing import Dict
 
-from bolna.ingestion_server.embeddings.openai_emded import OpenAI
-from bolna.ingestion_server.embeddings.base import BaseEmbed
+from embeddings.openai_emded import OpenAI
+from embeddings.base import BaseEmbed
 
 # EmbedProviders dictionary with the key being the name of the provider and the value being the class of the provider
 EmbedProviders : Dict[str, BaseEmbed] = {
--- rags/__init__.py
+++ rags/__init__.py
@@ -1,6 +1,6 @@
-from bolna.ingestion_server.rags.lancedb_rag import LanceDB
-from bolna.ingestion_server.rags.mongoDB_rag import MongoDB
-from bolna.ingestion_server.rags.base import BaseRAG
+from rags.lancedb_rag import LanceDB
+from rags.mongoDB_rag import MongoDB
+from rags.base import BaseRAG
 
 from typing import Dict
 
